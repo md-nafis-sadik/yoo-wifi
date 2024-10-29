b3fe9a2bfb8e8f91029e4a22ee64536c801720b3
hridoyhaque <hridoyhaque.info>
2024-10-29 17:32:20 +0600

    add not found and chenge router

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import NotFound from "../pages/NotFound";
 import { comRoutes } from "./comRoutes";
 import { corpRoutes } from "./corpRoutes";
+
 export const routes = createBrowserRouter([
   ...comRoutes,
   ...corpRoutes,
