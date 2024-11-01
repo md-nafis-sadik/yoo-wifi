fbc1aa083dd7fe3a418e43349bcdf31cab5d83a1
hridoyhaque <hridoyhaque.info>
2024-11-01 18:03:45 +0600

    add pocket wifi main home page

--- src/components/shared/cards/PackageCard.jsx
+++ src/components/shared/cards/PackageCard.jsx
@@ -1,25 +1,24 @@
 function PackageCard({ item = {}, ...props }) {
   return (
     <div className="px-4 py-5 rounded-xl border border-neutral-400 flex items-start gap-5">
-      <div className="w-14 aspect-square bg-main-50 rounded-lg flex items-center  justify-center">
+      <div className="w-14 aspect-square bg-main-50 p-1 rounded-lg flex items-center  justify-center">
         <img
-          src={item?.icon}
+          src={item?.image}
           alt="icon"
-          className="w-full h-full object-contain"
+          className="w-full h-full object-contain rounded"
         />
       </div>
       <div className="w-full flex items-end gap-2">
         <div className="w-full">
-          <h3 className="text-2xl font-bold text-black-700">
-            Global 50GB with Device
-          </h3>
-          <p className="text-base text-black-700">Keep Yoowifi Device</p>
-          <p className="text-base text-black-700">Top-up Plan only</p>
-          <p className="text-base text-black-700">365 Days Validity</p>
-          <p className="text-base text-black-700"> Covers 90 Countries</p>
+          <h3 className="text-2xl font-bold text-black-700">{item?.title}</h3>
+          {item?.features?.map((feature, index) => (
+            <p key={index} className="text-base text-black-700">
+              {feature}
+            </p>
+          ))}
         </div>
         <h4 className="text-2xl font-bold text-black-700 whitespace-nowrap">
-          SGD 79
+          {item?.price?.currency} {item?.price?.amount}
         </h4>
       </div>
     </div>
