5daca831c8bd9ed4822a842c336fee5b88ed1a94
hasibulofcs <hasibulofcs@gmail.com>
2024-11-04 18:47:37 +0600

    "updates on key features section"

--- src/components/shared/sections/KeyFeatures.jsx
+++ src/components/shared/sections/KeyFeatures.jsx
@@ -2,16 +2,19 @@ import FeatureCard from "@/components/shared/cards/FeatureCard";
 import SectionHeader from "@/components/shared/others/SectionHeader";
 import { cn } from "@/lib/utils";
 
-function KeyFeatures({ data = [], wrapperClass = "" }) {
+function KeyFeatures({ data = [], wrapperClass = "", showTitle = true }) {
   return (
     <section className="px-4">
       <div className="container3X sec_common_80 bg-neutral-100 rounded-3xl lg:px-12 xl:px-15">
         <div>
-          <SectionHeader
-            heading="Key Features"
-            subHeading="Don't let slow internet tie you down! Switch to our portable solutions to experience connectivity without boundaries."
-            containerClassName="gap-4"
-          />
+          {showTitle && (
+            <SectionHeader
+              heading="Key Features"
+              subHeading="Don't let slow internet tie you down! Switch to our portable solutions to experience connectivity without boundaries."
+              containerClassName="gap-4"
+            />
+          )}
+
           <div
             className={cn(
               "grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 px-4 mt-4 sm:mt-10 md:mt-15",
