d090af4aaa5b5bdeb412ea40ac0b664744bb2fd3
soumik9 <soumik.ahammed.9@gmail.com>
2024-11-05 16:02:35 +0600

    pasges & routes created corporate

---
+++ src/pages/corporate/Iot.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const Iot = () => {
+    return (
+        <Fragment>
+
+        </Fragment>
+    );
+};
+
+export default Iot;
---
+++ src/pages/corporate/MaritimeInternet.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const MaritimeInternet = () => {
+    return (
+        <Fragment>
+
+        </Fragment>
+    );
+};
+
+export default MaritimeInternet;
