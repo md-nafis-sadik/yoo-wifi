cce0f199502d9303c25f9ba8ebba682d5487d38e
soumik9 <soumik.ahammed.9@gmail.com>
2024-11-06 16:38:30 +0600

    lets talk social compnonent ui with responsive

--- src/store/module/contact/contactSlice.jsx
+++ src/store/module/contact/contactSlice.jsx
@@ -1,4 +1,4 @@
-import { images } from "@/services";
+import { FacebookIcon, images, InstagramIcon, LinkedInIcon, XIcon, YoutubeIcon } from "@/services";
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
@@ -18,6 +18,38 @@ const initialState = {
       image: images.email,
     },
   ],
+  socialLinks: [
+    {
+      _id: 1,
+      name: "Facebook",
+      link: "#",
+      icon: () => <FacebookIcon className="w-6 h-6 lg:w-8 lg:h-8" />
+    },
+    {
+      _id: 2,
+      name: "Youtube",
+      link: "#",
+      icon: () => <YoutubeIcon className="w-6 h-6 lg:w-8 lg:h-8" />
+    },
+    {
+      _id: 3,
+      name: "LinkedIn",
+      link: "#",
+      icon: () => <LinkedInIcon className="w-6 h-6 lg:w-8 lg:h-8" />
+    },
+    {
+      _id: 4,
+      name: "Instagram",
+      link: "#",
+      icon: () => <InstagramIcon className="w-6 h-6 lg:w-8 lg:h-8" />
+    },
+    {
+      _id: 5,
+      name: "X",
+      link: "#",
+      icon: () => <XIcon className="w-6 h-6 lg:w-8 lg:h-8" />
+    },
+  ]
 };
 
 const contactSlice = createSlice({
