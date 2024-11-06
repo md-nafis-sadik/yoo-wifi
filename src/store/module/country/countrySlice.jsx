5f19209c4696175bef36e811a6c257788576fbaf
soumik9 <soumik.ahammed.9@gmail.com>
2024-11-06 18:18:57 +0600

    Country coverage tabs, countries ui develop with responsive

--- src/store/module/country/countrySlice.jsx
+++ src/store/module/country/countrySlice.jsx
@@ -1,3 +1,4 @@
+import { images } from "@/services";
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
@@ -23,6 +24,68 @@ const initialState = {
             name: "Africa",
         },
     ],
+    countries: [
+        {
+            _id: 1,
+            name: "France",
+            image: images.FranceFlag,
+            region: "Europe"
+        },
+        {
+            _id: 2,
+            name: "Spain",
+            image: images.SpainFlag,
+            region: "Europe"
+        },
+        {
+            _id: 3,
+            name: "Italy",
+            image: images.ItalyFlag,
+            region: "Europe"
+        },
+        {
+            _id: 4,
+            name: "Germany",
+            image: images.GermanyFlag,
+            region: "Europe"
+        },
+        {
+            _id: 5,
+            name: "Greece",
+            image: images.GreeceFlag,
+            region: "Europe"
+        },
+        {
+            _id: 6,
+            name: "Belgium",
+            image: images.BelgiumFlag,
+            region: "Europe"
+        },
+        {
+            _id: 7,
+            name: "Croatia",
+            image: images.CroatiaFlag,
+            region: "Europe"
+        },
+        {
+            _id: 8,
+            name: "Ireland",
+            image: images.IrelandFlag,
+            region: "Europe"
+        },
+        {
+            _id: 9,
+            name: "Sweden",
+            image: images.SwedenFlag,
+            region: "Europe"
+        },
+        {
+            _id: 10,
+            name: "Switzerland",
+            image: images.SwitzerlandFlag,
+            region: "Europe"
+        },
+    ]
 };
 
 const countrySlice = createSlice({
