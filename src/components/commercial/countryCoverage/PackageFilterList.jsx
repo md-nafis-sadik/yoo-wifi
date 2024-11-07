b8ae85d6254b3b05220ef1ad1f2016c603851ff8
soumik9 <soumik.ahammed.9@gmail.com>
2024-11-07 11:38:27 +0600

    griding on packages list page

--- src/components/commercial/countryCoverage/PackageFilterList.jsx
+++ src/components/commercial/countryCoverage/PackageFilterList.jsx
@@ -17,11 +17,21 @@ const PackageFilterList = () => {
                 />
 
 
-                <div className='flex'>
-                    <div>
+                <div className='flex gap-8 mt-10'>
+                    <div className='w-[304px] p-6 border border-neutral-200 rounded-2xl'>
+                        <h4 className='text-black-900 text-[28px] font-extrabold leading-[120%]'>
+                            Filters
+                        </h4>
 
+                        <div className='my-8'>
+                            <input
+                                type="text"
+                                placeholder='Search anything...'
+                                className='w-full p-4 rounded-xl border border-neutral-300 outline-none bg-neutral-100 placeholder:text-black-600 text-black-900'
+                            />
+                        </div>
                     </div>
-                    <div>
+                    <div className='flex-1 grid grid-cols-2 gap-x-5 gap-y-4'>
                         {packages.map((data, index) => (
                             <InternetPackageCard
                                 key={index}
