a2867895d203b6b0e8413053ebde40b8a863003a
hasibulofcs <hasibulofcs@gmail.com>
2024-11-17 16:48:04 +0600

    updated contact us page components for translation

--- src/pages/commercial/Contact.jsx
+++ src/pages/commercial/Contact.jsx
@@ -1,18 +1,18 @@
 import LetsTalk from "@/components/commercial/contact/LetsTalk";
 import SupportAndFAQ from "@/components/commercial/contact/SupportAndFAQ";
 import HeroCommon from "@/components/shared/others/HeroCommon";
+import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 
 const Contact = () => {
   const { contact, faqs } = useSelector((state) => state.contact);
+  const { t } = useTranslation();
 
   return (
     <div>
       <HeroCommon
-        title={"Contact us"}
-        description={
-          "If you have a question or doubt, simply fill in the form and we will get back to you soon!"
-        }
+        title={t("contact.title")}
+        description={t("contact.description")}
       />
       <LetsTalk data={contact} />
       <SupportAndFAQ data={faqs} />
